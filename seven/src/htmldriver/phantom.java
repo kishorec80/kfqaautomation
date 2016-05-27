ade0f4f315b4d129a99277bf7313d7ccef66a673
kishorec80 <kishorec261980@gmail.com>
2016-05-27 17:51:18 +0530

    Removed unsed imports

--- seven/src/htmldriver/phantom.java
+++ seven/src/htmldriver/phantom.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;   
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
